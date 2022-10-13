e0337c8b9b300e4c610d7b3d3d98f5eb2c261564
LeandrodeLimaC <leandrolima7472@gmail.com>
2022-10-13 20:11:22 -0300

    First commit

---
+++ src/components/Heading.tsx
@@ -0,0 +1,24 @@
+import { Slot } from "@radix-ui/react-slot";
+import { clsx } from "clsx";
+
+export interface HeadingProps {
+  size?: "sm" | "md" | "lg";
+  children: React.ReactNode;
+  asChild?: boolean;
+}
+
+export function Heading({ size = "md", children, asChild }: HeadingProps) {
+  const Tag = asChild ? Slot : "h2";
+
+  return (
+    <Tag
+      className={clsx("text-gray-100 font-sans font-bold", {
+        "text-lg": size === "sm",
+        "text-xl": size === "md",
+        "text-2xl": size === "lg",
+      })}
+    >
+      {children}
+    </Tag>
+  );
+}
