5ed592f49e8948a1e28a74f4d1e9ce51c5890c64
LeandrodeLimaC <leandrolima7472@gmail.com>
2022-10-13 21:52:24 -0300

    fix: added className to components and created login screen

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -5,18 +5,28 @@ export interface HeadingProps {
   size?: "sm" | "md" | "lg";
   children: React.ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-export function Heading({ size = "md", children, asChild }: HeadingProps) {
+export function Heading({
+  size = "md",
+  children,
+  asChild,
+  className,
+}: HeadingProps) {
   const Tag = asChild ? Slot : "h2";
 
   return (
     <Tag
-      className={clsx("text-gray-100 font-sans font-bold", {
-        "text-lg": size === "sm",
-        "text-xl": size === "md",
-        "text-2xl": size === "lg",
-      })}
+      className={clsx(
+        "text-gray-100 font-sans font-bold",
+        {
+          "text-lg": size === "sm",
+          "text-xl": size === "md",
+          "text-2xl": size === "lg",
+        },
+        className
+      )}
     >
       {children}
     </Tag>
